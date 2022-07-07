1b82b84a6b7ee01a7410ce298f9de17278acde4a
iman <hashemi.iman@gmail.com>
2022-07-07 13:24:35 +0430

    mapping

---
+++ mapping.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity ^0.8.15;
+
+
+contract mapp{
+
+    mapping ( uint => string) _map;
+
+    function setMap(uint _key, string memory _value) public {
+        _map[_key]=_value;
+    }
+
+}
+
+    
+
